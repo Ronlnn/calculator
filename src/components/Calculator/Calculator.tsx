4a59cd11d1437a28b3e2363ac3c7a7ce0b4e7702
guinitum <artking01kg@gmail.com>
2025-06-11 12:41:35 +0300

    feat: Ввод с клавиатуры, рефактор кнопок

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -6,14 +6,14 @@ import Keyboard from "../Keyboard/Keyboard";
 const Calculator: React.FC = () => {
   const [input, setInput] = useState("");
 
-  const handleInput = e => {
-    setInput(input => input + e);
+  const handleInput = event => {
+    setInput(input => input + event);
     console.log(input);
   };
   return (
     <div className={styles.calculator}>
       <Display showInput={input} />
-      <Keyboard handleInput={handleInput} />
+      <Keyboard handleInput={handleInput} input={input} setInput={setInput}/>
     </div>
   );
 };
--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -1,30 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./keyboard.module.css";
 import Button from "../Button/Button";
+import { buttons } from "../../helpers/buttons";
 
-const Keyboard: React.FC = ({ handleInput }) => {
+const Keyboard: React.FC = ({ handleInput, input, setInput }) => {
+  //Ввод клавиатуры
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      const { key } = event;
+      if (/[0-9]/.test(key)) {
+        handleInput(key);
+      } else if (["+", "-", "*", "/"].includes(key)) {
+        handleInput(key);
+      } else if (key === "Enter") {
+        handleInput("=");
+      } else if (key === "Escape") {
+        handleInput("AC");
+      } else if (key === "." || key === ",") {
+        handleInput(",");
+      } else if (key === "Backspace") {
+        setInput(input => input.slice(0, -1));
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [input]);
   return (
     <div className={styles.keyboard}>
-      <Button value="AC" handleInput={handleInput} />
-      <Button value="+/-" handleInput={handleInput} />
-      <Button value="%" handleInput={handleInput} />
-      <Button value="/" handleInput={handleInput} isOrange="true" />
-      <Button value="7" handleInput={handleInput} />
-      <Button value="8" handleInput={handleInput} />
-      <Button value="9" handleInput={handleInput} />
-      <Button value="*" handleInput={handleInput} isOrange="true" />
-      <Button value="4" handleInput={handleInput} />
-      <Button value="5" handleInput={handleInput} />
-      <Button value="6" handleInput={handleInput} />
-      <Button value="-" handleInput={handleInput} isOrange="true" />
-      <Button value="1" handleInput={handleInput} />
-      <Button value="2" handleInput={handleInput} />
-      <Button value="3" handleInput={handleInput} />
-      <Button value="+" handleInput={handleInput} isOrange="true" />
-      <Button value="Sun" handleInput={handleInput} />
-      <Button value="0" handleInput={handleInput} />
-      <Button value="," handleInput={handleInput} />
-      <Button value="=" handleInput={handleInput} isOrange="true" />
+      {buttons.map(btn => (
+        <Button
+          key={btn.value}
+          value={btn.value}
+          handleInput={handleInput}
+          isOrange={btn.isOrange}
+        />
+      ))}
     </div>
   );
 };
